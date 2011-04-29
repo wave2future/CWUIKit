9593a8b12d21052c3dab0ca2e1c949dbfae7c16b
Fredrik Olsson <peylow@gmail.com>
2011-04-29 12:04:01 +0200

    Added missing header file, and new CWFoundation version

--- Classes/CWUIKit.h
+++ Classes/CWUIKit.h
@@ -1,5 +1,5 @@
 //
-//  CWAuxiliaryAction.h
+//  CWUIKit.h
 //  CWUIKit
 //  Created by Fredrik Olsson 
 //
@@ -28,44 +28,12 @@
 //  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 
-#import <UIKit/UIKit.h>
-
-@class CWAuxiliaryAction;
-typedef void (^CWAuxiliaryActionHandler)(CWAuxiliaryAction*);
-
-@interface CWAuxiliaryAction : NSObject {
-@private
-    NSString* _title;
-    CWAuxiliaryActionHandler _handler;
-    NSInteger _tag;
-}
-
-@property (nonatomic, copy, readonly) NSString* localizedTitle;
-@property (nonatomic, copy, readonly) CWAuxiliaryActionHandler handler;
-@property (nonatomic, assign) NSInteger tag;
-
-+(id)actionWithTitle:(NSString*)title handler:(CWAuxiliaryActionHandler)handler;
-+(id)actionWithTitle:(NSString*)title handler:(CWAuxiliaryActionHandler)handler tag:(NSInteger)tag;
--(id)initWithLocalizedTitle:(NSString*)title handler:(CWAuxiliaryActionHandler)handler;
-
-@end
-
-
-@interface UIAlertView (AuxiliaryAction)
-
-@property (nonatomic, copy) CWAuxiliaryActionHandler dismissHandler;
-
-+(UIAlertView*)alertViewWithTitle:(NSString*)title message:(NSString*)message auxiliaryActions:(NSArray*)actions cancelButtonTitle:(NSString*)cancelTitle;
-+(UIAlertView*)alertViewWithTitle:(NSString*)title message:(NSString*)message cancelButtonTitle:(NSString*)cancelTitle otherTitlesAndAuxiliaryActions:(NSString*)firstTitle, ... NS_REQUIRES_NIL_TERMINATION;
-
-@end
-
-
-@interface UIActionSheet (AuxiliaryAction)
-
-@property (nonatomic, copy) CWAuxiliaryActionHandler dismissHandler;
-
-+(UIActionSheet*)actionSheetWithAuxiliaryActions:(NSArray*)actions cancelButtonTitle:(NSString*)cancelTitle;
-+(UIActionSheet*)actionSheetWithCancelButtonTitle:(NSString*)cancelTitle otherTitlesAndAuxiliaryActions:(NSString*)firstTitle, ... NS_REQUIRES_NIL_TERMINATION;
-
-@end
+#import "CWAuxiliaryAction.h"
+#import "CWCalloutView.h"
+#import "CWLinearLayoutView.h"
+#import "CWPrimaryViewWindow.h"
+#import "NSObject+CWNibLocalizations.h"
+#import "UIAlertView+CWErrorHandler.h"
+#import "UIDevice+CWCapabilities.h"
+#import "UIImage+CWResize.h"
+#import "UIViewController+CWPopover.h"
